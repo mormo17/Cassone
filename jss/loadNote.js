20bd711d7155cc5ea6592b5ce983a812e932da63
mormo17 <mormo17@freeuni.edu.ge>
2020-11-15 23:48:36 +0400

    Initial Page of Diary added. Added functional to load diary’s html

---
+++ jss/loadNote.js
@@ -0,0 +1,11 @@
+function loadNote(color){
+    // console.log('ragaca');
+    // console.log(color);
+    if(color[0]=='#'){
+        color=color.replace('#', '')
+        location.href = `../htmls/diary.html?backgroundColor=${color},isSpecialColor=true`;
+    }else{
+        location.href = `../htmls/diary.html?backgroundColor=${color}`;
+    }
+    // console.log(location.href)
+}
