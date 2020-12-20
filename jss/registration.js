6ea3950bf261c992dba5f384b720de2afcb60cad
mormo17 <mormo17@freeuni.edu.ge>
2020-12-20 18:29:52 +0400

    Registration page fixed

--- jss/registration.js
+++ jss/registration.js
@@ -8,143 +8,53 @@ export default class RegistrationPage {
         }
 
         let content = `
-        <style type="text/css">
-            div {
-                padding: 15px;
-            }
-
-            body {
-                width: 60%;
-                margin: auto;
-            }
-
-            #testconfig {
-                width: 50%;
-                height: 300px;
-                position: absolute;
-                background: white;
-                z-index: 9;
-            }
-
-            input {
-                padding: 10px;
-            }
-            
-            p {
-                margin: 10px 0 10px 0;
-            }
-
-            .input-container {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-direction: column;
-                margin-left: 60px;
-                border: 2px solid #4CAF50;
-                background: white;
-            }
-
-            .pwd-container {
-                display: flex;
-                align-items: center;
-                justify-content: center;
-                flex-direction: row;
-            }
-
-            #register {
-                padding: 15px 32px;
-                border: none;
-                cursor: pointer;
-                border-radius: 4px;
-                color: white;
-                background-color: #4CAF50;
-            }
-
-            #register:hover {
-                background-color: #555555;
-            } 
-            .upload-image {
-                display: flex;
-                justify-content: center;
-                align-items: center;
-                flex-direction: column;
-            }
-            .load {
-                width: 50%;
-            }
-            .button {
-                border: none;
-                padding: 10px 15px;
-                cursor: pointer;
-                border-radius: 4px;
-                color: white;
-                background-color: #4CAF50;
-            }
-            .button:hover {
-                background-color: #555555;
-            }
-            .img-container {
-                padding: 0;
-                margin-top: 12px;
-                width: 200px;
-                height: 200px;
-                border: 2px solid black;
-                border-radius: 50%;
-            }
-            #output {
-                border-radius: 50%;
-            }
-        </style>
-
-        <div id="app">
-            <div class="input-container">
-                <h2> Registration </h2>
-
-                <div>
-                    <p> Username </p>
-                    <input type="text" 
-                        id="username" 
-                        placeholder="username">
-                    </input>
-                </div>
-                
-                <div>
-                    <p> Password </p>
-                    <input type="password" 
-                        id="password" 
-                        placeholder="password">
-                    </input>
-                </div>
-                
-                <div>
-                    <p> Repeat Password </p>
-                    <input type="password" 
-                        id="repeat_password" 
-                        placeholder="repeat password">
-                    </input>
-                </div>
-                
-                <button id="register">Register</button>
+        <link rel="stylesheet" href="../styles/initialPage.css">
+        <div id='signInForm'>
+            <div id='cassone'>
+                <img id="cassone" src="../images/cassone.png">
             </div>
+            <div id="greeting">
+                <div id="greetingText">Register To Open Your Cassone!</div>
+            </div>
+            <div id='emailPasswordContainer'>
+                <div id='emailBox'>
+                    <img id="emailIcon" src="../images/envelope.svg">
+                    <input type="text" id="emailInputArea" name="email" scroll="none" placeholder="User Name"></input>
+                </div>
+                <div id='passwordBox'>
+                    <img id="passwordIcon" src="../images/lock.svg">
+                    <input type="password" id="passwordInputArea" name="password" scroll="none" placeholder="Enter Password"></input>
+                </div>
+                <div id='passwordBox'>
+                    <img id="passwordIcon" src="../images/lock.svg">
+                    <input type="password" id="repeat_password" name="repeat_password" scroll="none" placeholder="Repeat Password"></input>
+                </div>
+            </div>
+            
+            <button id="register-btn">Register</button>
+            
+            <div id="versionText">Version</div>
+        </div>
+            <div id="error_message" style="color: red"> </div>
         </div>
         `;
         callback(content);
-
-        const register = document.getElementById('register');
+        
+        const register = document.getElementById('register-btn');
         register.addEventListener('click', () => this.register());
     }
-    
+
     static manageValidation(user, pwd, rep_pwd) {
         return (user.length > 0) && (pwd == rep_pwd) && (pwd.length > 0)
     }
     
+
     static register() {
-        var user = document.getElementById('username').value;
-        var password = document.getElementById('password').value;
+        var user = document.getElementById('emailInputArea').value;
+        var password = document.getElementById('passwordInputArea').value;
         var rep_password = document.getElementById('repeat_password').value;
     
         let userIsValid = this.manageValidation(user, password, rep_password);
-    
     
         if(userIsValid) {
             localStorage.setItem('user', JSON.stringify({userName: user, pwd: password}));
